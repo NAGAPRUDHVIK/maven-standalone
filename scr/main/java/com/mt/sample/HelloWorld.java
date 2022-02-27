f23ac7df73a2483e385218c48e3bcb1c83c2cb14
kkchari <kkchari530@gmail.com>
2022-02-27 20:06:59 +0530

    first commit to all java files

---
+++ scr/main/java/com/mt/sample/HelloWorld.java
@@ -0,0 +1,19 @@
+packge com.mt.sample;
+/**
+ *
+ *
+ * Hello world!
+ */
+import java.Util.Date;
+public class HelloWorld
+{
+	public Static void main(String[] args)
+	{
+		System.out.println("hello welcome to maven Bulid Tools!!today is:");
+	}
+	private static Date getLocal CurrentDate()
+	{
+		String m="mithun tech";
+		return new Date();
+	}
+}
